51bbb51a6d499a6744f7dba65ad5a782572501c6
crisbeto <crisbeto@abv.bg>
2016-12-20 22:19:12 +0200

    Update 2016-12-20T20:19:08.158Z

--- round-progress/round-progress.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class RoundProgressModule {
-}
-export { RoundProgressComponent } from './round-progress.component';
-export { RoundProgressService } from './round-progress.service';
-export { RoundProgressEase } from './round-progress.ease';
-export { RoundProgressConfig, RoundProgressDefaults } from './round-progress.config';
