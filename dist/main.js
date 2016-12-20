db065ecba5baa70e2f00a53aae3675fb45633558
crisbeto <crisbeto@abv.bg>
2016-12-20 21:32:31 +0200

    Update 2016-12-20T19:32:24.632Z

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var demo_module_1 = require('./demo.module');
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(demo_module_1.DemoModule);
+
+//# sourceMappingURL=main.js.map
