d84a90cb2d41305df1b90b67e6133511963c3359
Kristiyan Kostadinov <crisbeto@abv.bg>
2017-11-02 14:57:13 +0100

    chore: clear cache

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var demo_module_1 = require("./demo.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(demo_module_1.DemoModule);
-//# sourceMappingURL=main.js.map
